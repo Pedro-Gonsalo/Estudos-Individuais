4b1994011c96f7cf5da575edf864c7e520411a91
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2023-02-01 12:00:59 -0300

    Forwards, initialization e finalization

--- DELPHI/Curso da DevMedia/Básico do básico/Coisas_basicas.dpr
+++ DELPHI/Curso da DevMedia/Básico do básico/Coisas_basicas.dpr
@@ -2,13 +2,16 @@ program Coisas_basicas;
 
 uses
   Vcl.Forms,
-  uFormPrincipal in 'uFormPrincipal.pas' {Form1};
+  uFormPrincipal in 'uFormPrincipal.pas' {Form1},
+  uForm2 in 'uForm2.pas' {Form2},
+  uForWards in 'uForWards.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
