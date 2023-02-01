88fe024b4e73107b4b643e99b3d8075751142eda
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2023-02-01 16:07:43 -0300

    Tratamento de exceção

---
+++ DELPHI/Curso da DevMedia/Tratamento de Excecao/Tratamento_Excecao.dpr
@@ -0,0 +1,14 @@
+program Tratamento_Excecao;
+
+uses
+  Vcl.Forms,
+  uFormPrincipal in 'uFormPrincipal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
