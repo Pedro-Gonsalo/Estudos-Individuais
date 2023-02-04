197939b202d742c840d37c0470c59f27908d8b21
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2023-02-04 19:56:21 -0300

    Drag and Drop e ListBoxes

---
+++ DELPHI/Curso da DevMedia/Componentes da Toolbar/Drag and Drop e ListBoxes/Unit4.pas
@@ -0,0 +1,123 @@
+unit Unit4;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Buttons, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm4 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    lbxTecnologias: TListBox;
+    lbxHabilidades: TListBox;
+    BtnToHabilidade: TSpeedButton;
+    BtnToAllTecnologias: TSpeedButton;
+    BtnToAllHabilidade: TSpeedButton;
+    BtnToTecnologias: TSpeedButton;
+    Bevel1: TBevel;
+    BitBtn1: TBitBtn;
+    procedure BtnToHabilidadeClick(Sender: TObject);
+    procedure BtnToTecnologiasClick(Sender: TObject);
+    procedure BtnToAllHabilidadeClick(Sender: TObject);
+    procedure BtnToAllTecnologiasClick(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
+    procedure lbxTecnologiasDragDrop(Sender, Source: TObject; X, Y: Integer);
+    procedure lbxHabilidadesDragDrop(Sender, Source: TObject; X, Y: Integer);
+    procedure lbxTecnologiasDragOver(Sender, Source: TObject; X, Y: Integer;
+      State: TDragState; var Accept: Boolean);
+    procedure lbxHabilidadesDragOver(Sender, Source: TObject; X, Y: Integer;
+      State: TDragState; var Accept: Boolean);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm4.BitBtn1Click(Sender: TObject);
+begin
+close;
+end;
+
+procedure TForm4.BtnToAllHabilidadeClick(Sender: TObject);
+var
+  Item: String;
+begin
+  for Item in lbxTecnologias.Items do
+  begin
+    lbxHabilidades.Items.Append(Item);
+  end;
+  lbxTecnologias.Clear;
+end;
+
+procedure TForm4.BtnToAllTecnologiasClick(Sender: TObject);
+var
+  Item: String;
+begin
+  for Item in lbxHabilidades.Items do
+  begin
+    lbxTecnologias.Items.Append(Item);
+  end;
+  lbxHabilidades.Clear;
+end;
+
+procedure TForm4.BtnToHabilidadeClick(Sender: TObject);
+begin
+  if lbxTecnologias.ItemIndex > -1 then
+  begin
+    lbxHabilidades.Items.Add(lbxTecnologias.Items[lbxTecnologias.ItemIndex]);
+    lbxTecnologias.DeleteSelected;
+  end;
+end;
+
+procedure TForm4.BtnToTecnologiasClick(Sender: TObject);
+begin
+  if lbxHabilidades.ItemIndex > -1 then
+  begin
+    lbxTecnologias.Items.Add(lbxHabilidades.Items[lbxHabilidades.ItemIndex]);
+    lbxHabilidades.DeleteSelected;
+  end;
+end;
+
+procedure TForm4.lbxHabilidadesDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+  BtnToHabilidadeClick(sender);
+end;
+
+procedure TForm4.lbxHabilidadesDragOver(Sender, Source: TObject; X, Y: Integer;
+  State: TDragState; var Accept: Boolean);
+begin
+  Accept := Source <> Sender; // Só aceita o drop se a origem for diferente de quem está arrastando
+end;
+
+procedure TForm4.lbxTecnologiasDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+  BtnToTecnologiasClick(Sender); // Sender = Quem invocou este método
+end;
+
+procedure TForm4.lbxTecnologiasDragOver(Sender, Source: TObject; X, Y: Integer;
+  State: TDragState; var Accept: Boolean);
+begin
+  Accept := Source <> Sender;
+end;
+
+end.
+
+(* ANOTAÇÕES
+
+- Bevel: Exibe um quadro, painel ou caixa sobre um formulário ou componente-imagem com efeito
+tridimensional.
+
+- Drag and drop
+  - Propriedade: dragmode := autotomatic
+  - Eventos: dragdrop, dragover
+
+*)
