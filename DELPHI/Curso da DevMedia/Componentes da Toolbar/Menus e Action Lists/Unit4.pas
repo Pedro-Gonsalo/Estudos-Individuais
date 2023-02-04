790416cf1b1092ee0aea37f5b56acc8eeba8b305
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2023-02-04 18:42:46 -0300

    Menus e Action Lists

---
+++ DELPHI/Curso da DevMedia/Componentes da Toolbar/Menus e Action Lists/Unit4.pas
@@ -0,0 +1,68 @@
+unit Unit4;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.ImgList, Vcl.ActnList,
+  Vcl.StdCtrls, Vcl.ComCtrls, System.Actions, System.ImageList;
+
+type
+  TForm4 = class(TForm)
+    MainMenu1: TMainMenu;
+    Menu1: TMenuItem;
+    Item21: TMenuItem;
+    Salvar1: TMenuItem;
+    Edtar1: TMenuItem;
+    Copiar1: TMenuItem;
+    Colar1: TMenuItem;
+    N1: TMenuItem;
+    SalvarComo1: TMenuItem;
+    SalvarTodos1: TMenuItem;
+    Gravar1: TMenuItem;
+    ImageList1: TImageList;
+    Button1: TButton;
+    ActionList1: TActionList;
+    AcFechar: TAction;
+    Fechar1: TMenuItem;
+    StatusBar1: TStatusBar;
+    procedure Gravar1Click(Sender: TObject);
+    procedure AcFecharExecute(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm4.AcFecharExecute(Sender: TObject);
+begin
+  close;
+
+end;
+
+procedure TForm4.Gravar1Click(Sender: TObject);
+begin
+  if Gravar1.Checked then
+    ShowMessage('Gravação Iniciada')
+  else
+    ShowMessage('Gravação Interrompida');
+end;
+
+end.
+
+(* ANOTAÇÕES
+
+ - Componente Actionlist = cria ações que podem ser reaproveitadas, por exemplo: o botão fechar e o
+fechar do menu fazem a mesma coisa (fecham a aplicação), portanto, ao invés de colocar código no clique
+dos dois, é só criar a ação fechar e atribuilá à eles.
+
+
+
+*)
